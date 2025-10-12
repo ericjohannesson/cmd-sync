3716236add346b9afc52fa5f0064fc18e91da8af
eric <eric@ericjohannesson.com>
2025-10-12 02:56:34 +0200

    renamed function

--- cmd-sync.sh
+++ cmd-sync.sh
@@ -1,16 +1,44 @@
-display_syntax(){
-	echo "Syntax:"
-	echo "	bash cmd-sync.sh '<shell command containing expressions '\"\$IN\"' and '\"\$OUT\"'>' <path to source directory> <path to destination directory> [ dry-run ]"
-	echo ""
-	echo "Examples:"
-	echo "	bash cmd-sync.sh 'cp \"\$IN\" \"\$OUT\"' path/to/directory path/to/copied_directory"
-	echo "	bash cmd-sync.sh 'gpg --encrypt --recipient some@email.com --output \"\$OUT\" \"\$IN\"' path/to/directory path/to/encrypted_directory"
-	echo "	bash cmd-sync.sh 'gpg --decrypt --output \"\$OUT\" \"\$IN\"' path/to/encrypted_directory path/to/directory"
+#############################################################################
+# CMD-SYNC                                                                  #
+#                                                                           #
+# A bash-script for making the file structure of a *destination directory*  #
+# identical to the file structure of a *source directory* (without changing #
+# the source), where each destination file is the result of applying an     #
+# arbitrary *shell command* to the corresponding source file.               #
+#                                                                           #
+# The script uses GNU find for listing the path and modification-time of    #
+# each file, and GNU diff for determining the least amount of changes       #
+# required. If a file named `.cmd-sync-ignore` is present immediately       #
+# inside the *source directory*, each line of which is a regular expression #
+# that can be interpreted by GNU grep, the script will ignore files whose   #
+# path matches either of these expressions.                                 #
+#                                                                           #
+# Written by Eric Johannesson, eric@ericjohannesson.com                     #
+#############################################################################
+
+display_usage(){
+echo "Usage:"
+echo ""
+echo "  bash cmd-sync.sh"
+echo "      '<shell command containing expressions '\"\$IN\"' and '\"\$OUT\"'>'"
+echo "       <path to source directory>"
+echo "       <path to destination directory> [ dry-run ]"
+echo ""
+echo "Examples:"
+echo ""
+echo "- Make the destination identical to the source:"
+echo "  bash cmd-sync.sh 'cp \"\$IN\" \"\$OUT\"' path/to/directory path/to/copied_directory"
+echo ""
+echo "- Make the destination an encrypted version of the source:"
+echo "  bash cmd-sync.sh 'gpg --encrypt --recipient some@email.com --output \"\$OUT\" \"\$IN\"' path/to/directory path/to/encrypted_directory"
+echo ""
+echo "- Make the destination a decrypted version of the source:"
+echo "  bash cmd-sync.sh 'gpg --decrypt --output \"\$OUT\" \"\$IN\"' path/to/encrypted_directory path/to/directory"
 }
 
 if [ $# -lt 3 ]
 then
-	display_syntax
+	display_usage
 	exit
 fi
 
