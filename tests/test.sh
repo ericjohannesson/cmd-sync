e80a04ee5e2b40a8b9f7cedbcf81417b5de0e0fb
eric <eric@ericjohannesson.com>
2025-10-15 01:00:45 +0200

    testing dry-run

--- tests/test.sh
+++ tests/test.sh
@@ -7,6 +7,9 @@ bash ../cmd-sync.sh 'gpg --encrypt --batch --yes --no-tty --quiet --recipient er
 echo "#decrypt:"
 bash ../cmd-sync.sh 'gpg --decrypt --batch --yes --skip-verify --quiet --output "$OUT" "$IN"' output/encrypted_files output/files
 
+echo "#redact --dry-run:"
+bash ../cmd-sync.sh 'grep "hello" -v "$IN" > "$OUT"' input/files output/files --dry-run
+
 echo "# diff -r expected_output/files output/files:"
 diff -r expected_output/files output/files
 
